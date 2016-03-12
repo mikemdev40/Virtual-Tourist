0e887875c1785353c78b08fff2c32e88f2f3a625
mike miller <mikemdev40@gmail.com>
2016-03-11 23:31:53 -0500

    Feat: Add map view and ability to drop a pin on map with long press
    Utilized a long press gesture recognizer which was added to the map view

--- Virtual Tourist/ViewController.swift
+++ Virtual Tourist/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    struct Constants {
+        static let longPressDuration = 0.5
+    }
+    
+    @IBOutlet weak var mapView: MKMapView! {
+        didSet {
+            mapView.delegate = self
+            let longPress = UILongPressGestureRecognizer(target: self, action: "dropPin:")
+            longPress.minimumPressDuration = Constants.longPressDuration
+            mapView.addGestureRecognizer(longPress)
+        }
+    }
+    
+    func dropPin(gesture: UIGestureRecognizer) {
+        if gesture.state == .Began {
+            let pointPressed = gesture.locationInView(mapView)
+            let coordinate = mapView.convertPoint(pointPressed, toCoordinateFromView: mapView)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "Test"
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier("location") as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "location")
+            annotationView?.canShowCallout = true
+            annotationView?.pinTintColor = MKPinAnnotationView.redPinColor()
+          //  annotationView?.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+            
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        return annotationView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+     
+        
     }
 
     override func didReceiveMemoryWarning() {
