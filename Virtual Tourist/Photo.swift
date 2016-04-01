47e7e27bfde9157d58d483f1fc81630cc5b54410
mike miller <mikemdev40@gmail.com>
2016-03-31 20:37:25 -0400

    Doc: add comments to both NSManaged object classes

--- Virtual Tourist/Photo.swift
+++ Virtual Tourist/Photo.swift
@@ -10,16 +10,17 @@ import Foundation
 import CoreData
 import UIKit
 
-//in setting up this class in the data model:
-// - necessary to have the "ordered" box checked, so that it was possible to reference specific photos within the array (without "ordered" checked, calling something like "annotation.photos.first" causes a crash!  with ordering, it was possible to call .first)
+//in setting up this class in the data model, it was necessary to have the "ordered" box checked, so that it was possible to reference specific photos within the array (without "ordered" checked, calling something like "annotation.photos.first" causes a crash!  with ordering, it was possible to call .first)
 
 class Photo: NSManagedObject {
     
-    @NSManaged var photoID: String
-    @NSManaged var flickrURL: String
+    @NSManaged var photoID: String //this property is the Flickr objectID, a unique identifier that is returned by Flickr as part of the image data response
+    @NSManaged var flickrURL: String //this property is the URL for the image located on the web (as opposed to the URL to the image saved on disk)
+    
+    //the pin property implements the inverse one-to-one relationship that is defined in the core data model between a Photo and Pin; the property is marked as optional here since i decided that i wanted to make the class potentially initializable without a known associated pin at the time (although i ended up not implementing it this way; rather, the in the core data stack, the pin property gets set immediately after initializing an instance of this class, so it would have been just as practicel - if not more so - to make this a non-optional property and have it a part of the argument list of the init method below). in the core data model, however, this relationship is NOT marked as optional in the core data model, because under no circumstance do we want a photo persisting in the database (or saved on the disk) without a pin attached to it (otherwise there would be no way to access that photo!).  side note - i was curious what would happen if context.save() was called on an object of this class withOUT the pin property set, and it resulted in "Cocoa Error 1560" via an on screen device alert rather than a crash!
     @NSManaged var pin: PinAnnotation?
 
-    //computed property that returns the URL of the photo image file on disk (and in the cache); the URL is comprised of the documents directory and a unique file name which is based upoon the photoID (which is a unique identifier returned from Flickr)
+    //computed property that returns the URL of the photo image file on disk (and in the cache); the URL is comprised of the documents directory and a unique file name which is based upoon the unique photoID
     var photoURLonDisk: String? {
         return ImageFileManager.sharedInstance.getURLforFileOnDisk(photoID)
     }
@@ -27,17 +28,19 @@ class Photo: NSManagedObject {
     //computed property that, when accessed during collection view cell configuration, returns the saved value from the disk or returns nil; if this property returns nil when the cell calls for it, the cell congifuration then starts the downloading of the image which is then followed by setting this property equal to the freshly downladed image, thus invoking the setter observer below and the saving of the image to disk (and cache), which then leads to subsequent "get" calls to this property to return a non-nil image
     var photoImage: UIImage? {
         get {
-            return ImageFileManager.sharedInstance.retrieveImageFromDisk(photoURLonDisk, checkCacheForURL: flickrURL)
+            return ImageFileManager.sharedInstance.retrieveImageFromDisk(photoURLonDisk, checkCacheForURL: flickrURL)  //the "checkCacheForURL" argument is simply the web URL of the image, since the cache used in this project is the built-in caching mechanism that comes with the NSURL class, which utilizes the NSURLCache.sharedURLCache() instance and saves cache data based on the URL from which the object was downloaded
         }
         set {
-            ImageFileManager.sharedInstance.saveImageToDisk(newValue, photoURLonDisk: photoURLonDisk)
+            ImageFileManager.sharedInstance.saveImageToDisk(newValue, photoURLonDisk: photoURLonDisk)  //the photo being save (newvalue) is being passed along wih the computed URL for the image on the disk, and the image is then saved to files directory; the photos are set explicitly within the PhotoAlbumViewController class
         }
     }
     
+    //standard init that is required for loading data from core data on startup (without this, there will be a crash)
     override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
         super.init(entity: entity, insertIntoManagedObjectContext: context)
     }
     
+    //custom initializer that takes a NSManagedContext as part its pargument and calls the superclass initializer; see commment above for the pin property about why i decided to make the pin not part of the initiailization (after viewing how the implementation turned out, it would perhaps have made sense to just make it part of the initialization, since when each Photo instance gets created, the next line assigns the pin property, but that's not how i decided to implement it and since it doesn't affect the user experience either way, i just kept it this way
     init(photoID: String, flickrURL: String, context: NSManagedObjectContext) {
         let entity = NSEntityDescription.entityForName("Photo", inManagedObjectContext: context)!
         super.init(entity: entity, insertIntoManagedObjectContext: context)
@@ -46,6 +49,7 @@ class Photo: NSManagedObject {
         self.flickrURL = flickrURL
     }
     
+    //inherited NSManaged method that causes the related image files that are saved on the disk and in the cache
     override func prepareForDeletion() {
         ImageFileManager.sharedInstance.deleteImageFromDisk(photoURLonDisk, checkCacheForURL: flickrURL)
     }
--- Virtual Tourist/PinAnnotation.swift
+++ Virtual Tourist/PinAnnotation.swift
@@ -10,25 +10,30 @@ import Foundation
 import MapKit
 import CoreData
 
-class PinAnnotation: NSManagedObject, MKAnnotation {  //when "upgrading" this class to an NSManaged class, it was neccessary to remove the inheritance from NSObject (which was initially added to allow for MKAnnotation protocol conformance); i wanted to have this class conform to the MKAnnotation protocol so that these objects, when returned from core data, could be used immediately as annotations on the map (rather than having to create an intermediate set of annotations from objects of a non-MKannotation conforming class).
+class PinAnnotation: NSManagedObject, MKAnnotation {  //when "upgrading" this class to an NSManaged class (i started with a non-Core Data MKAnnotation-conforming object and converted it to Core Data later in development), it was neccessary to remove the inheritance from NSObject (which was initially added to allow for MKAnnotation protocol conformance); i wanted to have this class conform to the MKAnnotation protocol so that these objects, when returned from core data, could be used immediately as annotations on the map (rather than having to create an intermediate set of annotations from objects of a non-MKannotation conforming class).
     
+    //see comment below for the computed "coordinate" property for the rationale on why latitude and longitude were saved separately, rather than a single CLLocationCoordinate2D property
     @NSManaged var latitude: Double
     @NSManaged var longitude: Double
     
-    //the title and subtitle properties were included in the PinAnnotation class as part of the MKAnnotation protocol, more for completeness than anything (as neither are not required properties for the protocol); neither property was used in this project, but instead of deleting these from the class, i kept them for the purpose of possible expansion at a later time; for the time being, each PinAnnoation object is initialized with title and subtitle as nil
+    //the title and subtitle properties below were included in the PinAnnotation class as part of the MKAnnotation protocol, more for completeness than anything (as neither are not required properties for the protocol); neither property was used in this project, but instead of deleting these from the class, i kept them for the purpose of possible expansion at a later time; for the time being, each PinAnnoation object is initialized with title and subtitle as nil
     @NSManaged var title: String?
     @NSManaged var subtitle: String?
+    
+    //the photos property implements the one-to-many relationship that is defined in the core data model between a Pin and Photos; the property is marked as optional here since the pin object gets initialized with no initial photos (they are added later); this could alternatively been made non-optional with 0 being the default value (and value when no photos are returned from the flickr search); in the core data model, this relationship is also marked as optional in the core data model, because it is possible to persist the object with this relationship being nil (i.e. having no photos) (again, could alternatively have made it non optional with default 0)
     @NSManaged var photos: [Photo]?
     
-    //coordinate is required for the MKAnnotation protocol so it was included, but it needed to be a computer property because CLLocationCoordinate2D was not a storable type in core data; however, Double is, so it was possible to store latitude and longitude and then computer the coordinate from those values
+    //coordinate is required for the MKAnnotation protocol so it was included, but it needed to be a computed property because CLLocationCoordinate2D was not a storable type in core data (being a struct, it does not form to NSCoding, which is required for use of a Core Data "transformable" type); however, Double is, so it was possible to store latitude and longitude and then compute the required CLLocationCoordinate2D coordinate from those values!  it should be noted that it would have, alternatively, been possible to utilize a transformable "location" property that is of type "CLLocation" in this class, since CLLocation DOES conform to NSCoding, and then just pull out the CLLocationCoordinate2D coordiate property from that CLLocation!  (in essence, this would be using a CLLocation object as a wrapper for the desired CLLocationCoordinate2D in the Core Data persistent store)
     var coordinate: CLLocationCoordinate2D {
         return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
     
+    //standard init that is required for loading data from core data on startup (without this, there will be a crash)
     override init(entity: NSEntityDescription, insertIntoManagedObjectContext context: NSManagedObjectContext?) {
         super.init(entity: entity, insertIntoManagedObjectContext: context)
     }
     
+    //custom initializer that takes a NSManagedContext as part its pargument and calls the superclass initializer
     init(latitude: Double, longitude: Double, title: String?, subtitle: String?, context: NSManagedObjectContext) {
         
         let entity = NSEntityDescription.entityForName("Pin", inManagedObjectContext: context)!
